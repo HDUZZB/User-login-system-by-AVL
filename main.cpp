7898aadba0eca2e4a4c4105790422508893341de
HDUZZB <49432961+HDUZZB@users.noreply.github.com>
2020-02-05 00:00:47 +0800

    主函数

---
+++ main.cpp
@@ -0,0 +1,14 @@
+﻿
+#include "Landing.cpp"
+#include <time.h>
+#include <string>
+#include <stdlib.h>
+
+int main()
+{
+	system("color 70"); // 设置控制台背景颜色
+	Landing land;
+	int fst = 0;
+	while(!land.welcome(fst++));
+	return 0;
+}
