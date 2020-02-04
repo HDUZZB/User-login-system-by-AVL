a9caff0f9c83171123d8ea6b751d7612e28db306
HDUZZB <49432961+HDUZZB@users.noreply.github.com>
2020-02-04 23:58:46 +0800

    用户类的实现

---
+++ User.cpp
@@ -0,0 +1,24 @@
+﻿
+#include "User.h"
+
+User::User() {}
+User::User(string a,string b)
+{
+	id = a;
+	pass = b;
+}
+bool operator < (const User & t1,const User & t2) // 按 id 比较大小
+{
+	return t1.id < t2.id;
+}
+bool operator == (const User & t1,const User & t2) // id 相等，就算相等
+{
+	return t1.id == t2.id;
+}
+ostream& operator<<(ostream& out,const User& us)
+{
+	return out<<us.id;
+}
+
+
+
